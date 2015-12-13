2494e0e8911ad8877a8ddcb6c7118b7e7ecf6382
n0ir <n0ir@n0ir-MacBook-Air.local>
2015-12-13 22:51:14 +0900

    temporary handling to update the message

--- Entry_application/app/src/main/java/com/door2field/entryapplication/SubActivity.java
+++ Entry_application/app/src/main/java/com/door2field/entryapplication/SubActivity.java
@@ -2,6 +2,8 @@ package com.door2field.entryapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,14 +12,37 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
+
+    private TextView mTvMsg            = null;
+    private static int MSG_WHAT        = 1;
+    private static int MSG_INTERVAL_US = 100000;
+    private String pMsgAll             = null;
+    private int MsgLength              = 0;
+    private int WordIdx                = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        initMsg();
+    }
+
+    public void initMsg() {
+        mTvMsg    = (TextView)findViewById(R.id.MyTextView4Message);
+        /*
+         * @attention
+         * should be checked
+         */
+        pMsgAll   = new String(mTvMsg.getText().toString());
+        MsgLength = mTvMsg.length();
+        mTvMsg.setText("");
     }
 
     public void enterMainWorld(View v) {
@@ -46,4 +71,27 @@ public class SubActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void updateMsg(View v) {
+        Log.e("tagSubW", "countUp c4lled!");
+
+        handler.sendEmptyMessage(MSG_WHAT);
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void dispatchMessage(Message msg) {
+            if (WordIdx < MsgLength) {
+                if (msg.what == MSG_WHAT) {
+                    mTvMsg.setText(pMsgAll.substring(0, WordIdx + 1));
+                    handler.sendEmptyMessageDelayed(MSG_WHAT, MSG_INTERVAL_US / 1000);
+                    WordIdx++;
+                } else {
+                    super.dispatchMessage(msg);
+                }
+            } else {
+                pMsgAll = null;
+            }
+        }
+    };
 }
